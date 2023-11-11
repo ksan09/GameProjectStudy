8ed262ca1e069c083486ea4937389fa49158fe56
ksan09 <kansan060223@gmail.com>
2023-11-11 13:58:37 +0900

    Update CollisionMgr.cpp

--- 2-3WinAPIGameProject/2023_winapi_framework(2-3)/CollisionMgr.cpp
+++ 2-3WinAPIGameProject/2023_winapi_framework(2-3)/CollisionMgr.cpp
@@ -13,6 +13,11 @@ void CollisionMgr::CheckGroup(OBJ_GROUP _eLeft, OBJ_GROUP _eRight)
 	if (Row > Col)
 		std::swap(Row, Col);
 
+	// 만약에 체크가 이미 되어있다면?
+	if (m_arrCheck[Row] && (1 << Col))
+	{
+		m_arrCheck[Row] &= ~(1 << Col);
+	}
 
 	m_arrCheck[Row] |= (1 << Col);
 }
